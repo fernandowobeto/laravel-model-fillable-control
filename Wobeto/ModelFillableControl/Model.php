c6dcb8d587a34cd41b123cd88ee118f6f7aa1b72
Fernando WObeto <fernandowobeto@gmail.com>
2021-05-31 15:37:08 -0300

    model file

---
+++ Wobeto/ModelFillableControl/Model.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wobeto\ModelFillableControl;
+
+use Illuminate\Database\Eloquent\Model as LaravelModel;
+use Illuminate\Support\Arr;
+
+class Model extends LaravelModel
+{
+
+    protected $fillable = [];
+
+    public function __construct(array $attributes = [])
+    {
+        return parent::__construct(Arr::only($attributes, $this->fillable));
+    }
+
+    public function __set($key, $value)
+    {
+        if ($this->hasFillableAttribute($key)) {
+            return parent::__set($key, $value);
+        }
+    }
+
+    private function hasFillableAttribute(string $attribute): bool
+    {
+        return array_search($attribute, $this->fillable);
+    }
+
+}
